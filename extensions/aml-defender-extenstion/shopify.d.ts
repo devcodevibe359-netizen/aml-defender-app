8fec7a3ff4f1a08b77ec5f34f8008ff868906a37
Dev Codevibe <devcodevibe@gmail.com>
2026-05-22 16:01:59 +0530

    extenstion configration fixed

--- extensions/aml-defender-extenstion/shopify.d.ts
+++ extensions/aml-defender-extenstion/shopify.d.ts
@@ -1,7 +1,13 @@
 import '@shopify/ui-extensions';
 
 //@ts-ignore
-declare module './src/Checkout.jsx' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+declare module './src/Checkout.tsx' {
+  const shopify: import('@shopify/ui-extensions/purchase.thank-you.block.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
+
+//@ts-ignore
+declare module './src/services/QRgenerated.ts' {
+  const shopify: import('@shopify/ui-extensions/purchase.thank-you.block.render').Api;
   const globalThis: { shopify: typeof shopify };
 }
