d6e2a06ada19997c6e9a809a094fc41a8a38d143
Dev Codevibe <devcodevibe@gmail.com>
2026-05-26 12:08:06 +0530

    hfhbhf

--- app/routes/api.generate-qr.ts
+++ app/routes/api.generate-qr.ts
@@ -1,5 +1,6 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "react-router";
 import { authenticate, unauthenticated } from "../shopify.server";
+import prisma from "../db.server";
 import QRCode from "qrcode";
 import crypto from "crypto";
 
@@ -27,7 +28,66 @@ export async function action({ request }: ActionFunctionArgs) {
   try { 
     const {sessionToken } = await authenticate.public.customerAccount(request);
     const body = (await request.json()) as QRRequestBody;
+    const orderIdentityId = body.orderDetails;
     const token = crypto.randomUUID();
+ /* code start*/
+
+    if (!orderIdentityId) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: "Order identity missing",
+        }),
+        {
+          status: 400,
+          headers,
+        }
+      );
+    }
+
+    const existing = await prisma.kycVerification.findUnique({
+      where: {
+        orderIdentityId,
+      },
+    });
+    if (existing) {
+      const url = new URL(request.url);
+      const protocol =
+        request.headers.get("x-forwarded-proto") ||
+        url.protocol.replace(":", "");
+      const appDomain = `${protocol}://${url.host}`;
+      const kycUrl = `${appDomain}/kyc?token=${existing.kycToken}`;
+      const qrImage = await QRCode.toDataURL(kycUrl);
+      return new Response(
+        JSON.stringify({
+          success: true,
+          alreadyExists: true,
+          qrUrl: qrImage,
+          token: existing.kycToken,
+          redirectUrl: kycUrl,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            ...headers,
+          },
+        }
+      );
+    }
+    const kycToken = crypto.randomUUID();
+    const kycRecord = await prisma.kycVerification.create({
+      data: {
+        orderIdentityId,
+        kycToken,
+        kycStatus: false,
+        status: "PENDING",
+        reason: body.reason,
+      },
+    });
+
+
+/* end code  */
+
     const url = new URL(request.url);
     const protocol =  request.headers.get("x-forwarded-proto") || url.protocol.replace(":", "");
     const appDomain = `${protocol}://${url.host}`;
