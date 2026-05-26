dced0a3e65e585216f2ebae49d7f18d209cee691
Dev Codevibe <devcodevibe@gmail.com>
2026-05-26 14:09:40 +0530

    this is code for test

---
+++ app/routes/kky.tsx
@@ -0,0 +1,18 @@
+// app/routes/test.tsx
+
+export default function Test() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      Mobile Route Working ✅
+    </div>
+  );
+}
