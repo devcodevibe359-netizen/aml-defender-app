bad6b471a822bf57e3cc3241ba70378b676e29df
Dev Codevibe <devcodevibe@gmail.com>
2026-05-27 09:34:42 +0530

    new version updated

--- app/utils/uploadImage.server.js
+++ app/utils/uploadImage.server.js
@@ -1,14 +1,11 @@
 // app/utils/uploadImage.server.js
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3, BUCKET_NAME } from "./storage.server";
-import { v4 as uuidv4 } from "uuid";
 import path from "path";
 
-// ✅ Naya function — File object directly lo
 export async function uploadFileToRailway(file, prefix = "uploads") {
   const ext = path.extname(file.name) || ".jpg";
-  const fileName = `${prefix}/${uuidv4()}${ext}`;
-  
+  const fileName = `${prefix}/${crypto.randomUUID()}${ext}`;
   const arrayBuffer = await file.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
 
@@ -20,8 +17,6 @@ export async function uploadFileToRailway(file, prefix = "uploads") {
       ContentType: file.type || "image/jpeg",
     })
   );
-
-  // Public URL return karo
   const imageUrl = `${process.env.RAILWAY_BUCKET_ENDPOINT_URL}/${BUCKET_NAME}/${fileName}`;
   return imageUrl;
 }
